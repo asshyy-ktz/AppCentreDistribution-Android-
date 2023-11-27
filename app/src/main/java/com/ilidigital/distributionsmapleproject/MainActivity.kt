4ac4a442fc8385b016a54f9fc417c697a431584c
Ismaiel Sharif <ismaiel@ili.digital>
2023-11-27 14:46:37 +0500

    App centre configured..

---
+++ app/src/main/java/com/ilidigital/distributionsmapleproject/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.ilidigital.distributionsmapleproject
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ilidigital.distributionsmapleproject.ui.theme.DistributionSmapleProjectTheme
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DistributionSmapleProjectTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+
+        AppCenter.start(
+            application, "37a34a15-796b-4cfd-9414-fca432188199",
+            Analytics::class.java, Crashes::class.java
+        )
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DistributionSmapleProjectTheme {
+        Greeting("Android")
+    }
+}
